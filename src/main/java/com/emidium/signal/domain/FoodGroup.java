156ccfe00405138b13faa451be8be81dc86da9ba
James McDonald <jamesmcdonald5th@gmail.com>
2018-09-13 16:13:14 -0600

    FIrst loading test for FoodGroup

--- src/main/java/com/emidium/signal/domain/FoodGroup.java
+++ src/main/java/com/emidium/signal/domain/FoodGroup.java
@@ -2,22 +2,45 @@ package com.emidium.signal.domain;
 
 public class FoodGroup {
 
-    private String id;
-    private String description;
+    private String code;
+    private String name;
 
-    public String getId() {
-        return id;
+    public FoodGroup(String foodGroupCode, String foodGroupName) {
+        this.code = foodGroupCode;
+        this.name = foodGroupName;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getCode() {
+        return code;
     }
 
-    public String getDescription() {
-        return description;
+    public void setCode(String code) {
+        this.code = code;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FoodGroup foodGroup = (FoodGroup) o;
+
+        if (code != null ? !code.equals(foodGroup.code) : foodGroup.code != null) return false;
+        return name != null ? name.equals(foodGroup.name) : foodGroup.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = code != null ? code.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
--- src/test/java/com/emidium/signal/domainload/LoadFoodGroupsTest.java
+++ src/test/java/com/emidium/signal/domainload/LoadFoodGroupsTest.java
@@ -3,15 +3,18 @@ import com.emidium.signal.ResourcesTest;
 import com.emidium.signal.domain.FoodGroup;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class LoadFoodGroupsTest extends ResourcesTest {
     @Test
-    public void testCanLoadFoodGroupsFromCsv() {
+    public void testCanLoadFoodGroupsFromActualCsvFile() throws IOException {
         FoodGroupsFromCsv sut = new FoodGroupsFromCsv();
-        List<FoodGroup> foodGroups = sut.Load(resourcePath("foodgroups-en_ONPP.csv"));
+        List<FoodGroup> foodGroups = new ArrayList<>();
+        sut.Load(resourcePath("foodgroups-en_ONPP.csv")).forEach(foodGroups::add);
         assertEquals("Test file has 9 lines but 4 food groups.", 4, foodGroups.size());
     }
 
